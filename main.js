471a2b898b7931d46c043c5ac586bf4761fb11f6
Steph Hoel <steph.hoel@gmail.com>
2023-02-06 11:08:49 -0300

    Page v1.1 (ajustes finos)

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const firstNames = ['Alberta (f)', 'Asriel (m)', 'Astrid (f)', 'Audrey (u)', 'Billie (u) (American)', 'Zakarya (f)', 'Dante (m)', 'Deckard (m)', 'Gaitan (m)', 'Geoff (m)', 'Georgia (f)', 'Gladion (m)', 'Guili (u)', 'Joel (m)', 'Keith (u)', 'Kion (m)', 'Laury (f)', 'Levi (m)', 'Lillie (f)', 'Lusamine (f)', 'Lyra (f)', 'Mabarak (m)', 'Mallow (u)', 'Mark (m)', 'Mikhail (m) (German)', 'Mukhtar (m) (African)', 'Rael (u)', 'Raphäelle (f) (France)', 'Rauani (f)', 'Rauni (f)', 'Rhea (f)', 'Sadako (f)', 'Todrick (m)', 'Ulric (m) (German)']
+
 const hawaianNames = ['Anela (f)', 'Oliana (f)', 'Akoni (m)', 'Kai (m)', 'Kaleo (m)', 'Kalani (f)', 'Malia (f)', 'Nalu (f)']
-const lastNames = ['Beckham(English)', 'Haustrain', 'Jeha', 'Liers', 'MacCord', 'Monfrini', 'Nielsen(German)']
+
+const lastNames = ['Beckham (English)', 'Haustrain', 'Jeha', 'Liers', 'MacCord', 'Monfrini', 'Nielsen (German)']
 
 let delay = -0.4;
 function createListNames(listNames, title) {
